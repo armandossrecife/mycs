89993d4572c279198e1f7e0860233cb274ee2a64
Armando Soares Sousa <armando.sousa@gmail.com>
2023-03-28 15:28:51 +0000

    Refatorado para melhorar constantes do programa.

--- teste.py
+++ teste.py
@@ -1,15 +1,15 @@
 import datetime
 import os
 
-now = datetime.datetime.now()
-limpar_tela = 'clear'
-comando = 'ls -l'
+LIMPAR_TELA = 'clear'
+COMANDO = 'ls -l'
 
-os.system(limpar_tela)
+now = datetime.datetime.now()
+
+os.system(LIMPAR_TELA)
 
 print("Testando o code space no github")
 print(f'Data e hora atual: {now}')
 
-
 print('Lista os arquivos do diretório corrente: ')
-os.system(comando)
+os.system(COMANDO)
